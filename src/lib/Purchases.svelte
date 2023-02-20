133587a43b289a52f34eef1e7d999b392a15f325
caleb <33111047+CalebCress@users.noreply.github.com>
2023-02-20 14:13:45 -0500

    purchases rendering

--- src/lib/Purchases.svelte
+++ src/lib/Purchases.svelte
@@ -1,13 +1,32 @@
 <script>
+    import Icon from 'svelte-awesome';
+    import chevronRight from 'svelte-awesome/icons/chevronRight';
+
     export let transactions;
-    let name
+
+    let name;
+    let show = false;
+    let loading = false;
+
+    let iconOpen = false;
+
     if (transactions) {
         name = transactions[0].respresentative;
     }
-    console.log(transactions)
+    
+    function dropDown() {
+        iconOpen = !iconOpen;
+        loading = !loading;
+        setTimeout(()=> {
+            show = !show;
+        })
+    }
+
 </script>
-{#if transactions}
-    <h3>Purchases</h3>
+<h3 on:keydown={dropDown} on:click={dropDown} class="purchaseTitle">Purchases
+    <Icon data={chevronRight} style={`transform:rotate(${iconOpen ? 90 : 0}deg); transition: transform 150ms linear`}/>
+</h3>
+{#if show}
     <table class="table">
         <thead>
             <tr>
@@ -47,3 +66,13 @@
         </tbody>
     </table>
 {/if}
+{#if loading && !show}
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+{/if}
+<style>
+    .purchaseTitle {
+        cursor: pointer;
+    }
+</style>
