14d046bd857ea56f556cb0878193c43c551d6de1
caleb <33111047+CalebCress@users.noreply.github.com>
2023-02-20 12:11:57 -0500

    Added representative selection

--- src/lib/Representatives.svelte
+++ src/lib/Representatives.svelte
@@ -1,17 +1,31 @@
 <script>
     export let data;
+    export let currentRepData;
 
     let representatives = [];
-
+    let rep;
     data.forEach(transaction => {
         if (!representatives.includes(transaction.representative)) {
             representatives.push(transaction.representative)
         }
     })
+
+    function getRepData() {
+        currentRepData = [];
+        data.forEach(transaction => {
+            if (transaction.representative === rep) {
+                currentRepData.push(transaction)
+            }
+        })
+        currentRepData = currentRepData;
+    }
 </script>
 
 <main>
-    {#each representatives as representative}
-        <p>{representative}</p>
-    {/each}
+    <select bind:value={rep} class="form-select" aria-label="Default select example">
+        <option disabled selected>Select Representative</option>
+        {#each representatives as representative}
+            <option value={representative}>{representative}</option>
+        {/each}
+    </select>
 </main>
