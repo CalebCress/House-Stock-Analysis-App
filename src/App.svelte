38f007e38483ca49e555096cf122a20f143c7008
caleb <33111047+CalebCress@users.noreply.github.com>
2023-02-20 12:00:33 -0500

    New Project!

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Representatives from './lib/Representatives.svelte';
+  async function getData() {
+		const response = await fetch("https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json")
+    const resData = await response.json();
+
+		if (response.ok) {
+			return resData;
+		} else {
+			throw new Error(resData);
+		}
+	}
+
+  let data = getData()
+</script>
+<main>
+  {#await data}
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  {:then trans}
+    <Representatives data={trans}/>
+  {:catch error}
+    <div class="alert alert-danger" role="alert">
+      {error.message}
+    </div>
+  {/await}
+</main>
+
+<style>
+</style>
---
+++ src/lib/Representatives.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let data;
+
+    let representatives = [];
+
+    data.forEach(transaction => {
+        if (!representatives.includes(transaction.representative)) {
+            representatives.push(transaction.representative)
+        }
+    })
+</script>
+
+<main>
+    {#each representatives as representative}
+        <p>{representative}</p>
+    {/each}
+</main>
